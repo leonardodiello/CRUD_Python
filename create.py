29ee35f506c58eee0fb90e23fc7f12dc59f6b321
Leonardo Diello <leonardodiello4@gmail.com>
2026-09-09 09:22:53 -0400

    Usando boas práticas

---
+++ create.py
@@ -0,0 +1,17 @@
+from conexao import conectar
+
+
+conexao = conectar()
+cursor = conexao.cursor()
+
+nome_produto = "Farofa"
+valor = 4
+
+comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+
+cursor.execute(comando)
+
+conexao.commit()
+
+cursor.close()
+conexao.close()
