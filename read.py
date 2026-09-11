37ba5817d74ae1dea7dbbfadab68dbd4ffbf5813
Leonardo Diello <leonardodiello4@gmail.com>
2026-09-11 18:59:42 -0400

    melhorando o read, cada tupla retorna em uma linha

--- read.py
+++ read.py
@@ -7,7 +7,9 @@ comando = 'SELECT * FROM vendas'
 cursor.execute(comando)
 
 resultado = cursor.fetchall()
-print(resultado)
+
+for linha in resultado:
+    print(f'ID: {linha[0]} | Nome do Produto: {linha[1]} | Valor: {linha[2]}')
 
 cursor.close()
 conexao.close()
